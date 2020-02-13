d9ae4588093f5836beae61e9c23660a823160a73
Jakub Kwasek <jakub.kwasek@gmail.com>
2020-02-13 21:22:35 +0100

    add: function to styled component

--- src/components/styled.ts
+++ src/components/styled.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-export const SimpleWidgetStyled = styled.div`
-	width: 300px;
-	height: 100px;
-	display: block;
-	border: 1px solid ${(props): string => props.theme.colors.secondary};
-	background: ${(props): string => props.theme.colors.primary};
-	padding: 12px;
-	border-radius: 5px;
-`;
+export const SimpleWidgetStyled = styled.div(
+	(props) => `
+		width: 300px;
+		height: 100px;
+		display: block;
+		border: 1px solid ${props.theme.colors.secondary};
+		background: ${props.theme.colors.primary};
+		padding: 12px;
+		border-radius: 5px;
+`);
